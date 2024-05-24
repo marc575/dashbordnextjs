8596c4441dfcd5dddebb15d6210c06e58aecdea7
marc575 <mtatchou7@gmail.com>
2024-05-24 13:38:49 +0100

    recherche & pagination

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -17,7 +17,12 @@ const iconMap = {
 
 export default async function CardWrapper() {
   
-  const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
+  const { 
+    numberOfCustomers, 
+    numberOfInvoices, 
+    totalPaidInvoices, 
+    totalPendingInvoices 
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
